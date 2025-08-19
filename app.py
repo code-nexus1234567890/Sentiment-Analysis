faea77c951e0e0b81234a6fa97cae91698e5505d
code-nexus1234567890 <ayushmishra1809@gmail.com>
2025-08-20 00:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if not st.session_state.logged_in:
                 st.session_state.logged_in = True
                 st.session_state.username = username
                 st.success("Login successful ✅")
-                st.experimental_rerun()
+                st.rerun()
             else:
                 st.error("Invalid credentials ❌")
 
